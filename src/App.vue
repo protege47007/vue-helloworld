f5da761e303ba26825fcfba56987111d17e2fd68
protege47007 <davidak.a@yahoo.com>
2022-03-10 02:37:04 +0100

    initial commit -vue hello world

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" @click="toggleClose"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :display="display" @close="toggleClose">
+      <template v-slot:form>
+      <form ref="form" @submit="form">
+        <input type="text" ref="name" placeholder="name">
+        <br>
+        <input type="text" ref="post" placeholder="Position">
+        <br>
+        <input type="number" ref="xp" placeholder="years of experience">
+        <br>
+        <button type="submit">Post</button>
+      </form>
+      </template>
+      <h3>Name: {{nom}}</h3>
+      <h3>Vote for your next: {{position}}</h3>
+      <h4>He has {{xyz}} years of experience</h4>
+    </HelloWorld>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data: () => ({
+    nom: 'John Doe',
+    position: 'Janitor',
+    xyz: 999,
+    display: false,
+
+  }),
+  methods: {
+    form(e){
+      e.preventDefault();
+      this.nom = this.$refs.name.value
+      this.position = this.$refs.post.value
+      this.xyz = this.$refs.xp.value
+    },
+    
+    toggleClose(){
+      this.display = !this.display
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container" @click.self="toggleClose" v-if="display">
+    <div class="modal">
+      <h1>Hello World Vue II</h1>
+      <p>
+        Let's get this stuff.. top of the world, top of the world, top of the
+        world!
+      </p>
+      <slot></slot>
+      <slot name="form"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+    display: Boolean,
+  },
+  methods: {
+    toggleClose() {
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  position: absolute;
+  top: 0;
+  background-color:rgba(230, 230, 230, .5);
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.modal {
+  background: tomato;
+  width: 45%;
+  padding: 4rem;
+  margin: 10rem auto;
+  z-index: 10 !important;
+  opacity: 1 !important;
 }
 </style>
